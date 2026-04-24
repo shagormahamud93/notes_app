6b08dafa6731d837efcfb90f31c11989a42129a1
sagor ali <mahamudshagor93@gmail.com>
2026-04-25 02:41:09 +0600

    add splash screen, improve UI and finalize app for submission

--- lib/splash_page.dart
+++ lib/splash_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:notes_app/core/theme/app_colors.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -25,8 +26,54 @@ class _SplashPageState extends State<SplashPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Notes App", style: TextStyle(fontSize: 30))),
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(gradient: AppColors.mainGradient),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(25),
+                decoration: BoxDecoration(
+                  color: AppColors.glass,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: const Icon(
+                  Icons.note_alt_rounded,
+                  size: 80,
+                  color: AppColors.white,
+                ),
+              ),
+              const SizedBox(height: 25),
+              const Text(
+                "My Notes",
+                style: TextStyle(
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.white,
+                  letterSpacing: 1.2,
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                "Organize your thoughts easily",
+                style: TextStyle(fontSize: 14, color: AppColors.white70),
+              ),
+              const SizedBox(height: 30),
+              const Text(
+                "Organize your thoughts easily",
+                style: TextStyle(fontSize: 14, color: AppColors.white70),
+              ),
+              // Loading indicator
+              const CircularProgressIndicator(
+                color: AppColors.white,
+                strokeWidth: 2,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
